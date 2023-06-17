a6b07064fc77ee9c989e6fa31f9470aeb5fd365f
sagawritescode <45040561+sagawritescode@users.noreply.github.com>
2023-06-17 07:08:59 +0530

    Create model4.py

---
+++ Assignment7/model5/model4.py
@@ -0,0 +1,93 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+# Target - To increase accuracy. Using batch normalization and dropout
+# Result - train 99.2 test 99.32
+# Analysis - The accuracy has improved significantly due to batch normalization. We applied a small value for dropout 1%. 
+# But from the training logs we can clearly see that training accuracy keeps improving but testing doesnt. At one point training even crosses test. This is clear case of overfitting. Further experimentations required
+
+class Model4(nn.Module):
+    def __init__(self):
+        super(Model4, self).__init__()
+        dropout_value = 0.03
+        # Input Block
+        self.convblock1 = nn.Sequential(
+            nn.Conv2d(in_channels=1, out_channels=10, kernel_size=(3, 3), padding=0, bias=False),
+            nn.ReLU(),
+            nn.BatchNorm2d(10),
+            nn.Dropout(dropout_value)
+        ) # output_size = 26
+
+        # CONVOLUTION BLOCK 1
+        self.convblock2 = nn.Sequential(
+            nn.Conv2d(in_channels=10, out_channels=10, kernel_size=(3, 3), padding=0, bias=False),
+            nn.ReLU(),
+            nn.BatchNorm2d(10),
+            nn.Dropout(dropout_value)
+        ) # output_size = 24
+
+        self.convblock3 = nn.Sequential(
+            nn.Conv2d(in_channels=10, out_channels=16, kernel_size=(3, 3), padding=0, bias=False),
+            nn.ReLU(),
+            nn.BatchNorm2d(16),
+            nn.Dropout(dropout_value)
+        ) # output_size = 22
+
+
+        # TRANSITION BLOCK 1
+        self.convblock4 = nn.Sequential(
+            nn.Conv2d(in_channels=16, out_channels=10, kernel_size=(1, 1), padding=0, bias=False),
+            nn.ReLU()
+        ) # output_size = 22
+        self.pool1 = nn.MaxPool2d(2, 2) # output_size = 11
+
+        
+        # CONVOLUTION BLOCK 2
+        self.convblock5 = nn.Sequential(
+            nn.Conv2d(in_channels=10, out_channels=10, kernel_size=(3, 3), padding=0, bias=False),
+            nn.ReLU(),
+            nn.BatchNorm2d(10),
+            nn.Dropout(dropout_value)
+        ) # output_size = 9
+
+        self.convblock6 = nn.Sequential(
+            nn.Conv2d(in_channels=10, out_channels=16, kernel_size=(3, 3), padding=0, bias=False),
+            nn.ReLU(),
+            nn.BatchNorm2d(16),
+            nn.Dropout(dropout_value)
+        ) # output_size = 7
+
+        
+        self.convblock7 = nn.Sequential(
+            nn.Conv2d(in_channels=16, out_channels=16, kernel_size=(3, 3), padding=0, bias=False),
+            nn.ReLU(),
+            nn.BatchNorm2d(16),
+            nn.Dropout(dropout_value)
+        ) # output_size = 5
+
+        # OUTPUT BLOCK        
+        self.gap = nn.Sequential(
+            nn.AvgPool2d(kernel_size=5)
+        ) # output_size = 1
+
+        self.convblock8 = nn.Sequential(
+            nn.Conv2d(in_channels=16, out_channels=10, kernel_size=(1, 1), padding=0, bias=False),
+        ) # output_size = 1
+        
+
+
+    def forward(self, x):
+        x = self.convblock1(x)
+        x = self.convblock2(x)
+        x = self.convblock3(x)
+        x = self.convblock4(x)
+        x = self.pool1(x)
+        x = self.convblock5(x)
+        x = self.convblock6(x)
+        x = self.convblock7(x)
+        x = self.gap(x)
+        x = self.convblock8(x)
+        x = x.view(-1, 10)
+        return F.log_softmax(x, dim=-1)
+        
